16c7a92c111f298c128d60af1dfb85ac5938c45c
Richard Tang <Richard_L_Tang@mckinsey.com>
2022-10-29 20:48:35 +0700

    fix: deocrate premium

--- src/components/premium/Premium.tsx
+++ src/components/premium/Premium.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
-import { Center, Container } from '@chakra-ui/react'
+import './Premium.css'
+import premium from '../../assets/premium.svg'
 
 const Premium: React.FC<{}> = () => {
   const settings = {
@@ -14,37 +15,73 @@ const Premium: React.FC<{}> = () => {
   }
 
   return (
-    <Container mt="5" mb="5">
+    <div className="premium-container">
       <Slider {...settings}>
-        <Center
-          bg="yellow.100"
-          h="60px"
-          fontFamily="Average"
-          fontSize="20px"
-          fontWeight="400"
-        >
-          This is the content
-        </Center>
-        <Center
-          bg="blue.100"
-          h="60px"
-          fontFamily="Average"
-          fontSize="20px"
-          fontWeight="400"
-        >
-          This is the content
-        </Center>
-        <Center
-          bg="green.100"
-          h="60px"
-          fontFamily="Average"
-          fontSize="20px"
-          fontWeight="400"
-        >
-          This is the content
-        </Center>
+        <div>
+          <div className="premium-item" id="one">
+            <div className="premium-text">
+              Premium free for the next 3 Months
+            </div>
+            <img src={premium} alt="diamond" className="premium-img" />
+          </div>
+        </div>
+        <div>
+          <div className="premium-item" id="two">
+            <div className="premium-text">
+              Premium free for the next 3 Months
+            </div>
+            <img src={premium} alt="diamond" className="premium-img" />
+          </div>
+        </div>
+        <div>
+          <div className="premium-item" id="three">
+            <div className="premium-text">
+              Premium free for the next 3 Months
+            </div>
+            <img src={premium} alt="diamond" className="premium-img" />
+          </div>
+        </div>
+        <div>
+          <div className="premium-item" id="four">
+            <div className="premium-text">
+              Premium free for the next 3 Months
+            </div>
+            <img src={premium} alt="diamond" className="premium-img" />
+          </div>
+        </div>
       </Slider>
-    </Container>
+    </div>
+    // <Container mt="5" mb="5">
+    //   <Slider {...settings}>
+    //     <Center
+    //       bg="yellow.100"
+    //       h="60px"
+    //       fontFamily="Average"
+    //       fontSize="20px"
+    //       fontWeight="400"
+    //     >
+    //       This is the content
+    //     </Center>
+    //     <Center
+    //       bg="blue.100"
+    //       h="60px"
+    //       fontFamily="Average"
+    //       fontSize="20px"
+    //       fontWeight="400"
+    //     >
+    //       This is the content
+    //     </Center>
+    //     <Center
+    //       bg="green.100"
+    //       h="60px"
+    //       fontFamily="Average"
+    //       fontSize="20px"
+    //       fontWeight="400"
+    //     >
+    //       This is the content
+    //     </Center>
+    //   </Slider>
+    // </Container>
   )
 }
 
